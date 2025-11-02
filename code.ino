d63e13a3b87bc94ca664e5f2622eb8562de194db
박용성 <45269941+JosephStalin0305@users.noreply.github.com>
2025-11-02 06:13:35 +0000

    Adding BSW & ASW

--- code.ino
+++ code.ino
@@ -1,5 +1,51 @@
+volatile uint16_t adcValue = 0;
+volatile bool adcReady = false;
+
 void setup() {
+    Serial.begin(115200);
+
+    pinMode(2, OUTPUT);
+    pinMode(3, OUTPUT);
+    pinMode(4, OUTPUT);
+    pinMode(5, OUTPUT);
+    pinMode(6, OUTPUT);
+    pinMode(7, OUTPUT);
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+    pinMode(10, OUTPUT);
+    pinMode(11, OUTPUT);
+
+
+    ADMUX = 0;
+    ADMUX |= (1 << REFS0);
+    ADMUX |= 0; 
+
+    ADCSRA = 0;
+    ADCSRA |= (1 << ADEN);
+    ADCSRA |= (1 << ADIE);
+    ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+
+    sei();
+
+    ADCSRA |= (1 << ADSC);
 }
 
 void loop() {
+    if (adcReady) {
+        int value = map(adcValue, 0, 200, 0, 11);
+        value = min(11, value);
+        for (int i = 2; i < 10+2; i++) {
+            digitalWrite(i, 0);
+        }
+        for (int i = 2; i < value+2; i++) {
+            digitalWrite(i, 1);
+        }
+        adcReady = false;
+    }
+}
+
+ISR(ADC_vect) {
+    adcValue = ADC;
+    adcReady = true;
+    ADCSRA |= (1 << ADSC);
 }
